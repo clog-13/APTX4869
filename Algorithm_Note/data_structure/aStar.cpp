f06657d227b3ec405d1914ad8a78a439a8ccb17d
xiiv13 <937846472@qq.com>
2020-10-11 01:42:49 +0800

    Kest path

--- Algorithm_Note/data_structure/aStar.cpp
+++ Algorithm_Note/data_structure/aStar.cpp
@@ -1,91 +1,70 @@
-#include <iostream>
-#include <cstring>
-#include <queue>
-#include <unordered_map>
-#include <algorithm>
+#include <bits/stdc++.h>
 
 using namespace std;
-typedef pair<int, string> PIS;
 
-unordered_map<string, int> dist;
-unordered_map<string, pair<string, char>> pre;
-priority_queue<PIS, vector<PIS>, greater<PIS>> heap;
-string ed = "12345678x";
-int dx[4] = {-1, 0, 1, 0};
-int dy[4] = { 0, 1, 0,-1};
-char dir[] = "urdl";
+struct edge {   // edge代表边的结构体
+    int e, v;
+};
 
-int getDist(string state) {   // 求估值函数,这里是曼哈顿距离
-    int res = 0;
-    for(int u = 0; u < 9; u++) {
-        if(state[u] != 'x') {
-            int c = state[u] - '1';
-            res += abs(c/3-u/3) + abs(c%3-u%3);
-        }
+struct node {   // A*内每个节点的node结构体
+    long long st,l,f;
+    bool operator<(const node&a) const {    // 重载小于号规定优先队列的排序为l+f(p)(也就是当前的总长度与估价函数值的和)
+        return a.l+a.f < l+f;
     }
-    return res;
-}
+};
 
-string bfs(string start) {
-    heap.push({getDist(start) , start});
-    dist[start] = 0;
+vector<edge> G[1005],GG[1005];  // 定义两个存图的vector,G是正向的,GG是逆向的.因为是有向图所以逆向SPFA需要用逆向存图的GG
+const int inf = 0x3f3f3f3f;  // SPFA用的inf
+int main() {
+    long long dis[1005], bk[1005];   // dis代表SPFA的单源最短路的记录,bk是SPFA过程中的辅助数组
+    int n, m;
+    scanf("%d%d", &n, &m);
+    while (m--) {
+        int s, e, v;
+        scanf("%d%d%d", &s, &e, &v);
+        G[s].push_back((edge){e, v});//模拟邻接表存图
+        GG[e].push_back((edge){s, v});
+    }
+    int st, ed, k;
+    scanf("%d%d%d", &st, &ed, &k);
 
-    while (heap.size()) {
-        auto cur = heap.top();
-        heap.pop();
-
-        string state = cur.second;
-        if(state == ed) break;  // 如果到达终点就break
-        int step = dist[state]; // 记录到达state的实际距离
-
-        int k = state.find('x');// 确定当前 'x' 的位置
-        int x = k / 3 , y = k % 3;
-
-        string backup = state;  // 因为在下面state会变，所以留一个备份
-        for (int i = 0; i < 4; i++) {
-            int tx = x + dx[i], ty = y + dy[i];
-            if (tx >= 0 && tx < 3 && ty >= 0 && ty < 3) {
-                swap(state[x*3+y], state[tx*3+ty]);
-
-                if (!dist.count(state) || dist[state] > step + 1) {
-                    dist[state] = step + 1;
-                    pre[state] = {backup, dir[i]};
-                    heap.push({dist[state] + getDist(state), state});
+    // 逆向SPFA
+    for (int i = 1; i <= n; i++) dis[i] = i == ed ? 0 : inf;
+    queue<int> q;
+    q.push(ed);
+    while (q.size()) {
+        int cur = q.front(); 
+        q.pop();
+        bk[cur] = 0;
+        for (int i = 0; i < GG[cur].size(); i++) {
+            if (dis[GG[cur][i].e] > dis[cur]+GG[cur][i].v) {
+                dis[GG[cur][i].e] = dis[cur]+GG[cur][i].v;
+                if (!bk[GG[cur][i].e]) {
+                    bk[GG[cur][i].e] = 1;
+                    q.push(GG[cur][i].e);
                 }
-                swap(state[x * 3 + y], state[tx * 3 + ty]);   // 因为要多次交换，所以要恢复现场
             }
         }
-
     }
+    
+    for (int i = 0; i <= n; i++) printf("%d\n", dis[i]);
 
-    string res;
-    while (ed != start) {
-        res += pre[ed].second;
-        ed = pre[ed].first;
-    }
-    reverse(res.begin() , res.end());
-    return res;
-}
-
-int main() {
-    string init;
-    char c;
-    for (int i = 0; i < 9; i++) {
-        cin >> c;
-        init += c;
-    }
-
-    int cnt = 0;
-    for(int i = 0; i < 9; i++) {
-        if (init[i] == 'x') continue;
-        for(int j = i+1; j < 8; j++) {
-            if (init[j] == 'x') continue;
-            if(init[i] > init[j]) cnt++;             
+    // aStar
+    if (dis[st] == inf) return printf("-1"), 0;
+    priority_queue<node> Q;
+    Q.push((node){st, 0, 0});
+    int cntt = 0;
+    if (ed == st) k++;
+    while (Q.size()) {
+        node p = Q.top();
+        Q.pop();
+        if (p.st == ed) {
+          cntt++;
+          if (cntt == k) return printf("%lld", p.l), 0;
+        }
+        for (int i = 0; i < G[p.st].size(); i++) {
+            Q.push((node){G[p.st][i].e, G[p.st][i].v+p.l, dis[G[p.st][i].e]});
         }
     }
-
-    if(cnt % 2) puts("unsolvable");
-    else cout<<bfs(init)<<endl;
-
-    return 0;
+    printf("-1");
 }
