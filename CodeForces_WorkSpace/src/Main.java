496a60c101b939e39dd138dac4013267781a2cfe
xiiv13 <937846472@qq.com>
2020-11-08 22:32:04 +0800

    just coding

--- CodeForces_WorkSpace/src/Main.java
+++ CodeForces_WorkSpace/src/Main.java
@@ -1,20 +1,71 @@
+import java.util.*;
 import java.io.*;
 
-public class Main {
+class Main {
+    static int N, idx = 0, maxN = 1510;
+    static int[] from = new int[maxN], to = new int[maxN], info = new int[maxN];
+    static boolean[] hasPa = new boolean[maxN];
+    static int[][] dp = new int[maxN][3];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[3];
-        for (int i = 0; i < N; i++) {
-            String[] str = br.readLine().split(" ");
-            arr[0] += Integer.parseInt(str[0]);
-            arr[1] += Integer.parseInt(str[1]);
-            arr[2] += Integer.parseInt(str[2]);
+        while (true) {
+            String str = br.readLine();
+            if (str == null) break;
+            N = Integer.parseInt(str);
+
+            Arrays.fill(hasPa, false);
+            Arrays.fill(info, -1);
+            idx = 0;
+            while (N-- > 0) {
+                String[] arr = br.readLine().split(" ");
+                int a = Integer.parseInt(arr[0].substring(0, arr[0].indexOf(':')));
+                for (int i = 1; i < arr.length; i++) {
+                    int b = Integer.parseInt(arr[i]);
+                    add(a, b);
+                    hasPa[b] = true;
+                }
+            }
+
+            int root = 0;
+            while (hasPa[root]) root++;
+            dfs(root);
+            System.out.println(Math.min(dp[root][1], dp[root][2]));
         }
-        boolean flag = true;
-        for (int a : arr) if (a != 0) flag = false;
-        if (flag) System.out.println("YES");
-        else System.out.println("NO");
+    }
+
+    private static void dfs(int u) {
+        dp[u][1] = 1;
+        int sum = 0;
+        for(int i = info[u]; i != -1; i = from[i]) {
+            int t = to[i];
+            dfs(t);
+
+            dp[u][0] += Math.min(dp[t][1], dp[t][2]);
+            dp[u][1] += Math.min(dp[t][0], Math.min(dp[t][1], dp[t][2]));
+            sum += Math.min(dp[t][1], dp[t][2]);
+        }
+
+        dp[u][2] = 0x3f3f3f3f;
+        for(int i = info[u]; i != -1; i = from[i]) {
+            int t = to[i];
+            dp[u][2] = Math.min(dp[u][2], dp[t][1] + sum - Math.min(dp[t][1], dp[t][2]));
+        }
+    }
+
+    // private static void dfs(int u) {
+    //     dp[1][u] = 1; dp[0][u] = 0;
+    //     for (int i = info[u]; i != -1; i = from[i]) {
+    //         int t = to[i];
+    //         dfs(t);
+    //         dp[0][u] += dp[1][t];
+    //         dp[1][u] += Math.min(dp[1][t], dp[0][t]);
+    //     }
+    // }
+
+    private static void add(int a, int b) {
+        from[idx] = info[a];
+        to[idx] = b;
+        info[a] = idx++;
     }
 }
--- CodeForces_WorkSpace/src/Solution.java
+++ CodeForces_WorkSpace/src/Solution.java
@@ -1,28 +1,51 @@
+import java.net.Socket;
+import java.util.*;
+
 class Solution {
 
     public static void main(String[] args) {
+        String t = "1.0.1.";
+        String[] str = t.split("\\.", -1);
+        String[] str1 = t.split("\\.");
+
+        System.out.println();
+
         Solution sol = new Solution();
-        int[] arr = {37,69,3,74,46};
-        int[][] pieces = {{37,69,3,74,46}};
-        sol.canFormArray(arr, pieces);
+        int[] arr = {497978859,167261111,483575207,591815159};  // 373219333
+        System.out.println(sol.maxProfit(arr, 836556809));
     }
 
-    public boolean canFormArray(int[] arr, int[][] pieces) {
-        boolean[] vis = new boolean[pieces.length];
-        outer: for (int i = 0; i < arr.length; ) {
-            for (int j = 0; j < pieces.length; j++) {
-                if (vis[j]) continue;
-                if (pieces[j][0] == arr[i]) {
-                    vis[j] = true;
-                    for (int k = 0; k < pieces[j].length; k++) {
-                        if (arr[i+k] != pieces[j][k]) return false;
-                    }
-                    i += pieces.length;
-                    continue outer;
-                }
+    public int maxProfit(int[] arr, int orders) {
+        int N = arr.length, MOD = 1000000007;
+
+        Arrays.sort(arr);
+        long res = 0;
+        while (true) {
+            int le = N-1, ri = N-1;
+            while (le > 0 && arr[le-1] == arr[ri]) le--;    // 宽
+            int w = ri-le+1;
+            int nex = le == 0 ? 0 : arr[le-1];
+            int h = arr[le]-nex;
+
+            if (orders <= w*h) {
+                int th = orders/w;
+
+                long t = (arr[ri]%MOD-th+1)%MOD;
+                long tmp = (th*(t+arr[ri]%MOD)%MOD) % MOD;
+                res = (res + ((tmp/2)%MOD * w)%MOD) % MOD;
+                orders -= w*th;
+                if (orders != 0) for (int j = le; j <= ri; j++) arr[j] -= th;
+                for (int i = 0; i < orders; i++) res = (res + arr[ri]) % MOD;
+                break;
+            } else {
+                long t = (nex+1+arr[ri])%MOD;
+                long tmp = (h*t) % MOD;
+                res = (res + ((tmp/2)%MOD * w)%MOD) % MOD;
+                orders -= w*h;
+                for (int i = le; i <= ri; i++) arr[i] = nex;
             }
-            return false;
         }
-        return true;
+
+        return (int)res%MOD;
     }
 }
