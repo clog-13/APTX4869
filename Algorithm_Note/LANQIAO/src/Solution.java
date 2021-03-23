4f51f5b64c329a40639e96175ddabd763a1da9db
Xiiv13 <937846472@qq.com>
2021-03-23 20:49:33 +0800

    java file modifed

--- Algorithm_Note/LANQIAO/src/Solution.java
+++ Algorithm_Note/LANQIAO/src/Solution.java
@@ -1,38 +1,45 @@
-class Solution {
+import java.util.*;
 
+class Solution {
     public static void main(String[] args) {
         Solution sol = new Solution();
-        int[][] m = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
-        sol.spiralOrder(m);
+//        sol.maxValue(6, 1, 10);
+//        sol.maxValue(4, 0, 4);
+//        sol.maxValue(9, 3, 16);
+        sol.maxValue(1, 0, 24);
     }
 
-    public int[] spiralOrder(int[][] mat) {
-        int row = mat.length, col = mat[0].length;
-        int le = 0, ri = col - 1, to = 0, bo = row - 1;
-        int[] res = new int[row * col];
-        int idx = 0, tar = row * col;
-        while (idx < tar) {
-            for (int i = le; i <= ri; i++) {
-                res[idx++] = mat[to][i]; // left to right.
-                if (idx >= tar) break;
+    public int maxValue(int n, int idx, int maxSum) {
+        int le = 0, ri = maxSum+1;
+        while (le < ri) {
+            int mid = (le+ri)>>1;
+            if (check((long)mid, (long)n, (long)idx) <= (long)maxSum) {
+                le = mid+1;
+            } else {
+                ri = mid;
             }
-            to++;
-            for (int i = to; i <= bo; i++) {
-                res[idx++] = mat[i][ri]; // top to bottom.
-                if (idx >= tar) break;
-            }
-            ri--;
-            for (int i = ri; i >= le; i--) {
-                res[idx++] = mat[bo][i]; // right to left.
-                if (idx >= tar) break;
-            }
-            bo--;
-            for (int i = bo; i >= to; i--) {
-                res[idx++] = mat[i][le]; // bottom to top.
-                if (idx >= tar) break;
-            }
-            le++;
         }
-        return res;
+        return le-1;
+    }
+
+    long check(long x, long n, long idx) {
+        if (x==0) return 0;
+        long lc = idx+1, rc = n-idx;
+        long ls = 0, rs = 0;
+        if (x > lc) {
+            ls = lc*(x-lc+1 + x) / 2;
+        } else if (x <= lc) {
+            ls = x*(1 + x) / 2;
+            ls += idx-x+1;
+        }
+
+        if (x > rc) {
+            rs = rc*(x-rc+1 + x) / 2;
+        } else if (x <= rc) {
+            rs = x*(1 + x) / 2;
+            rs += n-idx-x;
+        }
+        long sum = ls + rs - x;
+        return sum;
     }
 }
